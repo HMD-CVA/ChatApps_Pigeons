3427a9b5af1377d33d0c9c0689b8cefddce34bcb
HM Duck <hmduck@MacBook-Air-cua-HM.local>
2026-01-24 13:12:22 +0700

    Fix Error Deploy Render

--- client/src/app/services/user.ts
+++ client/src/app/services/user.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class User {
-  private apiUrl = 'http://localhost:8080/admin/users';
+  private apiUrl = `${environment.apiUrl}/admin/users`;
 
   constructor(private http: HttpClient) { }
 
---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://chatapps-pigeons-server.onrender.com'  // URL backend của bạn
+};
